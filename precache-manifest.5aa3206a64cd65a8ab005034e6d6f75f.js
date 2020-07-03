86a3a20afb8f28ad02754648fe511cead60785c7
Marmik Shah <marmikshah@icloud.com>
2020-07-03 19:07:37 +0800

    Init

---
+++ precache-manifest.5aa3206a64cd65a8ab005034e6d6f75f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8950f4228e564f06fc2ca66293d456c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b158b3848c4c1fe211f5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "11f3b4455f52c9dcb721",
+    "url": "/static/js/2.fbaa21b2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fbaa21b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b158b3848c4c1fe211f5",
+    "url": "/static/js/main.200cc5e3.chunk.js"
+  },
+  {
+    "revision": "00f84fe2f94790ba93b7",
+    "url": "/static/js/runtime-main.a5d61b70.js"
+  }
+]);
